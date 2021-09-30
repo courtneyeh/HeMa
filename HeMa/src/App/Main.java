6bb30cf1f8d39472b2c53a60aeb475e352e4b3e8
glow250 <49109384+glow250@users.noreply.github.com>
2021-10-01 09:55:13 +1300

    original changes to remove numbers from predictiosn

--- HeMa/src/App/Main.java
+++ HeMa/src/App/Main.java
@@ -1,7 +1,10 @@
 package App;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         String evaluationDir = args[0];
         int numThreads = Integer.parseInt(args[1]);
         String dataDir = args[2];
